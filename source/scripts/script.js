a630226889b1699b3af979fcd71c6ec76b82df06
Stepan Vasilyev <intellectualspecialist@gmail.com>
2024-01-04 16:08:01 +0500

    Вносит исправления

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -4,7 +4,7 @@ const navBurger = document.querySelector('.main-nav__burger');
 siteList.classList.add('main-nav__list--closed');
 navBurger.classList.remove('main-nav__burger--active');
 
-navBurger.addEventListener('click', function () {
+navBurger.addEventListener('click', () => {
   if (siteList.classList.contains('main-nav__list--closed')) {
     siteList.classList.remove('main-nav__list--closed');
     navBurger.classList.add('main-nav__burger--active');
